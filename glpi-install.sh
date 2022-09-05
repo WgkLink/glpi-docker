5aacc0423e800aceff949f8b4c5ef5405466d241
William <linkwgk@gmail.com>
2022-09-05 09:54:10 -0300

    modified:   .gitignore 	modified:   docker-compose.yml 	modified:   glpi-install.sh

--- glpi-install.sh
+++ glpi-install.sh
@@ -16,4 +16,13 @@ find /var/www/glpi -type f -exec chmod 644 {} \;
 
 
 /etc/init.d/php8.1-fpm start
+
+# Configuração do banco de dados
+php /var/www/glpi/bin/console glpi:database:update --db-host=$GLPI_DB_HOST --db-name=$GLPI_DB_NAME --db-user=$GLPI_DB_USER --db-password=$GLPI_DB_PASSWORD
+
+# Reajustando o acesso ao usuário do Apache
+chown www-data. /var/www/html/glpi/files -Rf
+
 nginx -g 'daemon off;'
+
+echo 'GLPI installed'
